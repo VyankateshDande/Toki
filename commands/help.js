94f83e55d62f8734ba28fa3201c06807160fd71c
VyankateshDande <85945388+VyankateshDande@users.noreply.github.com>
2022-04-19 23:24:31 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,7 @@ module.exports = {
     name:"help",
     execute(message, args, Discord, bot, axios, database, announcement){
         const help_embed = new Discord.MessageEmbed({
+            description:"Please replace `<` with your custom prefix in case you have set one for your server.",
             color:"#00ccff",
             timestamp:new Date(),
             author:{
@@ -15,6 +16,7 @@ module.exports = {
             fields: [
                 {name:"<help", value:"Provides a list of all commands and a short description of each command.", inline:false},
                 {name:"<ping", value:"Returns the bot latency", inline:false},
+                {name:"<setprefix [custom prefix]", value:"Set a custom prefix for your server! \n`Note`: This command can only be used by users who have `Manage Server` permission.", inline:false},
                 {name:"<server-info", value:"Provides information about server. \nProvided information includes but is not limited to number of roles, members, how many members are bots, how many members are humans, nad other information.", inline:false},
                 {name:"<user-info [user]", value:"Provides information about specified user. If no user is specified, provides information about the author of the command. \nProvided information inlcudes but is not limited to roles the user has in the server, permissions, account creation date with time, the date the user joined the server (with time), and other information", inline:false},
                 {name:"<role-info [role name]", value:"Provides information about the specified role. \nProvided information includes but is not limited to Permissions granted to the role, role creation date, color of role (represented by embed color), role ID, and other information.\nIf two roles have the same name, the role lower in the role list will be displayed.", inline:false},
