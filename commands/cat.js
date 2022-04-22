61e91db05e48d5585ecadf89121f04f6c8ce0fb9
VyankateshD <85945388+VyankateshDande@users.noreply.github.com>
2022-04-22 18:08:12 +0530

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -2,7 +2,7 @@ module.exports = {
     name:"cat",
     async execute(message, args, Discord, bot, axios, database, announcement){
         const cat_pic_req = await axios.get(('https://some-random-api.ml/img/cat'))
-        if (cat_pic_req.datalink == undefined){
+        if (cat_pic_req.data.link == undefined){
             message.reply("Something went wrong. Try again later.")
             return
         }
