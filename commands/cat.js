ba75a156a8b492f8002f7ca9c3c60157d6ba1881
VyankateshD <85945388+VyankateshDande@users.noreply.github.com>
2022-04-22 18:04:58 +0530

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -1,18 +1,17 @@
 module.exports = {
     name:"cat",
     async execute(message, args, Discord, bot, axios, database, announcement){
-        const cat_pic_req = await axios.get(('https://cataas.com/cat?json=true&tags=cute'))
-        if (cat_pic_req.data.url == undefined){
+        const cat_pic_req = await axios.get(('https://some-random-api.ml/img/cat'))
+        if (cat_pic_req.datalink == undefined){
             message.reply("Something went wrong. Try again later.")
             return
         }
-        const cat_pic = `https://cataas.com/${cat_pic_req.data.url}`
         const catpic_embed = new Discord.MessageEmbed({
             title:"cat <3",
             color:"#00ccff",
             timestamp:new Date(),
             image:{
-                url:cat_pic
+                url:cat_pic_req.data.link
             },
             footer:{
                 text:`Requested by ${message.author.tag}`,
